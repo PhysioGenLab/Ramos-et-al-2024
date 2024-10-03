c304b5ee0ff82cf6744017165334a3bdfb8725f6
PhysioGen Lab <physiogenlab@usp.br>
2024-10-03 15:16:05 -0500

    Uploading LSDC CLI tool command
    This file contains all the commands utilized to perform Global correlation analysis.

---
+++ LDSC/ADHDxTESTO_HOMENS_MULHRES_ESTRAD_HOMENS.sh
@@ -0,0 +1,5 @@
+./ldsc.py \
+--ref-ld-chr eur_w_ld_chr/ \
+--out /home/iago/ldsc/ADHDxTESTO_HOMENS_MULHRES_ESTRAD_HOMENS \
+--rg munge_data/ADHD.sumstats.gz,munge_data/TESTO_HOMENS.sumstats.gz,munge_data/TESTO_MULHERES.sumstats.gz,munge_data/TESTO_MULHERES_HOMENS.sumstats.gz,munge_data/ESTRAD_HOMENS.sumstats.gz \
+--w-ld-chr eur_w_ld_chr/ 
